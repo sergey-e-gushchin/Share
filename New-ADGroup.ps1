580bc55499d695b6a019bdbf4c2bc74e225e0d4b
Гущин Сергей <sergei.e.gushchin@csm.nov.ru>
2015-04-02 12:18:24 +0300

    *

--- New-ADGroup.ps1
+++ New-ADGroup.ps1
@@ -1,31 +1,62 @@
-﻿$path = 'OU=Новгородский филиал,OU=ФБУ \"Тест-С.-Петербург\",DC=test-csm,DC=nov,DC=ru' ;
-
-$ADGroup = Get-ADGroup `
-    -Filter {Name -eq "Персонал"} `
-    -SearchScope Subtree `
-    -SearchBase $path ;
-
-$GetADOrganizationalUnit = Get-ADOrganizationalUnit `
-    -Filter 'OU -notlike "Новгородский филиал"' `
-    -SearchScope Subtree `
-    -SearchBase $path ;
-$targetpath = [string] $GetADOrganizationalUnit.DistinguishedName ;
-
-$NewADGroup = New-ADGroup `
-    -Name "Персонал" `
-    -GroupCategory Security `
-    -GroupScope Global `
-    -DisplayName "Персонал" `
-    -SamAccountName Users `
-    -path $targetpath `
-    -PassThru ;
-    
-if($ADGroup) 
-    {
-        Write-Host "Группа(ы) уже существует(ют)"
+﻿Function New-ITGPersonalGroup {
+    [CmdletBinding(
+        SupportsShouldProcess = $true
+ 		, ConfirmImpact = 'Medium'
+    )] 
+    param (
+        [parameter(
+            Mandatory = $true
+            , ValueFromPipeline = $true
+            , ValueFromPipelineByPropertyName = $true
+        )]
+        [Alias ('DistinguishedName')]
+        [System.String] $OUDistinguishedName
+    )
+    process {
+        New-ADGroup `
+            -Name 'Персонал' `
+            -GroupCategory Security `
+            -GroupScope Global `
+            -path ( $OUDistinguishedName ) `
+            -SamAccountName "Персонал $(Get-ITGOUsamAccountNameSuffix $OUDistinguishedName)" `
+            -WhatIf:$WhatIfPreference `
+			-Verbose:$VerbosePreference `
+        ;
     }
-else
-    {
-        $NewADGroup
-        Write-Host "Группа(ы) успешно создана(ы)"
-    } 
+}
+
+[System.Text.RegularExpressions.Regex] $distNameComponent = ` 
+    '(?<currentObjectId>(?<type>\w+)=(?<Id>.*?)),(?<parentContainerId>.*)'
+
+Function Get-ITGOUsamAccountNameSuffix {
+    param (
+        [parameter(
+            Mandatory = $true
+            , ValueFromPipeline = $true
+            , ValueFromPipelineByPropertyName = $true
+        )]
+        $DistinguishedName
+    )
+    process {
+        $null = $DistinguishedName -match $distNameComponent;
+        if ( $Matches['type'] -ne 'OU' ) {
+            return '';
+        };
+        $OU = Get-ADOrganizationalUnit `
+            -Identity $DistinguishedName `
+            -Properties adminDisplayName
+        ;
+        $suffix = $OU.name;
+        if ( $OU.adminDisplayName ) {
+            $suffix = $OU.adminDisplayName
+        };
+        $parentId = $Matches['parentContainerId'];
+        $parentSuffix = Get-ITGOUsamAccountNameSuffix $parentId;
+        if ( $parentSuffix ) {
+            $suffix = "$parentSuffix-$suffix";
+        };
+        return $suffix;
+    }
+} 
+
+# Get-ADOrganizationalUnit -SearchBase 'OU=ФБУ \"Тест-С.-Петербург\",DC=test-csm,DC=nov,DC=ru' -SearchScope Subtree -Filter '*' | New-ITGPersonalGroup -Verbose -WhatIf
